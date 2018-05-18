d46f500ff4a78c08ab6599d8ced74259b1e5ba31
crasnencov <crasnencov@gmail.com>
2018-05-18 18:28:02 +0300

    Main scene 2

---
+++ Assets/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	public class Enemy : MonoBehaviour
+	{
+
+		GameObject target;
+		public float speed;
+		public float HP;
+
+
+
+		void Start()
+		{
+			target = GameObject.Find("Base");
+
+		}
+
+
+		void Update()
+		{
+			// healthBar.value = HP;
+			transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+		}
+
+
+	}
+
